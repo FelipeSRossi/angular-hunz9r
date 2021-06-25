1bf45de96a18963bc72590ed8290ef131086a13e
FelipeSRossi <felipe.rossi@ufv.br>
2021-06-25 19:09:23 -0300

    7 files modified, 2 added, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,42 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TableComponent } from './table/table.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { ChartModule } from 'angular2-chartjs';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CdkTableModule,
     ReactiveFormsModule,
     MatSidenavModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-    ])
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTableModule,
+    MatCardModule,
+    ChartModule,
+    RouterModule.forRoot([{ path: '', component: TableComponent }])
   ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    TableComponent,
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  declarations: [AppComponent, ProductListComponent, TableComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  data: Date;
+  demanda: number;
+  capacidade: number;
+  atendimento_planejado: number;
+  atendimento_realizado: number;
+  desvio: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    data: new Date(),
+    demanda: 1,
+    capacidade: 0,
+    atendimento_planejado: 2,
+    atendimento_realizado: 3,
+    desvio: 0
+  },
+  {
+    data: new Date(),
+    demanda: 1,
+    capacidade: 0,
+    atendimento_planejado: 2,
+    atendimento_realizado: 3,
+    desvio: 0
+  },
+  {
+    data: new Date(),
+    demanda: 1,
+    capacidade: 0,
+    atendimento_planejado: 2,
+    atendimento_realizado: 3,
+    desvio: 0
+  },
+  {
+    data: new Date(),
+    demanda: 1,
+    capacidade: 0,
+    atendimento_planejado: 2,
+    atendimento_realizado: 3,
+    desvio: 0
+  }
+];
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  displayedColumns: string[] = [
+    'data',
+    'demanda',
+    'capacidade',
+    'atendimento_planejado',
+    'atendimento_realizado',
+    'desvio'
+  ];
 
-  constructor() { }
+  dataSource = ELEMENT_DATA;
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
