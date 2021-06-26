267d8ef4c624649a9cde334cf04d8f625625b7b1
FelipeSRossi <felipe.rossi@ufv.br>
2021-06-26 09:49:30 -0300

    1 file modified, 1 added

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.css']
 })
 export class ChartComponent implements OnInit {
+  
+  type = 'line';
+  data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+  options = {
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
 
   constructor() { }
 
